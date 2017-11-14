93a55d420047a86fe2b8d190609d30940df08242
rumi <mujtaba.rumi1@gmail.com>
2017-11-14 18:33:59 +0600

    rumi

--- application/models/Admin/Ordersm.php
+++ application/models/Admin/Ordersm.php
@@ -9,9 +9,7 @@ class Ordersm extends CI_Model
         $this->db->from('orders o');
         $this->db->where('o.orderDate BETWEEN CURDATE() - INTERVAL 7 DAY AND CURDATE()');
         $this->db->join('orderitems oi','oi.fkOrderId = o.id','left');
-
         $this->db->join('users u','u.id = o.fkUserId','left');
-
         $query=$this->db->get();
         return $query->result();
     }
